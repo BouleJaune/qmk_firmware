cf184b71f12f941e96fc4eaec7ce5a36ebcc1daa
BouleJaune <amthierry81@live.fr>
2023-04-15 22:24:24 +0200

    compile script for sweepi0 and improvments

--- keyboards/sweepi0/config.h
+++ keyboards/sweepi0/config.h
@@ -23,20 +23,3 @@
 #        error EE_HANDS without left/right config
 #    endif
 #endif
-
-
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
-
-/* disable debug print */
-//#define NO_DEBUG
-
-/* disable print */
-//#define NO_PRINT
-
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
